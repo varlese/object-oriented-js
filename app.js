d0972404c63750d2c3284a386760927036b13747
varlese <1361760+varlese@users.noreply.github.com>
2021-11-20 15:16:58 -0500

    Create Dino constructor, parse JSON to create dino objects, create Human constructor function.

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
+    const dinosJson = '{"Dinos":[{"species":"Triceratops","weight":13000,"height":114,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"First discovered in 1889 by Othniel Charles Marsh"},{"species":"Tyrannosaurus Rex","weight":11905,"height":144,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"The largest known skull measures in at 5 feet long."},{"species":"Anklyosaurus","weight":10500,"height":55,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"Anklyosaurus survived for approximately 135 million years."},{"species":"Brachiosaurus","weight":70000,"height":"372","diet":"herbavor","where":"North America","when":"Late Jurasic","fact":"An asteroid was named 9954 Brachiosaurus in 1991."},{"species":"Stegosaurus","weight":11600,"height":79,"diet":"herbavor","where":"North America, Europe, Asia","when":"Late Jurasic to Early Cretaceous","fact":"The Stegosaurus had between 17 and 22 seperate places and flat spines."},{"species":"Elasmosaurus","weight":16000,"height":59,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Elasmosaurus was a marine reptile first discovered in Kansas."},{"species":"Pteranodon","weight":44,"height":20,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Actually a flying reptile, the Pteranodon is not a dinosaur."},{"species":"Pigeon","weight":0.5,"height":9,"diet":"herbavor","where":"World Wide","when":"Holocene","fact":"All birds are living dinosaurs."}]}';
+
+    const dinosData = JSON.parse(dinosJson);
 
     // Create Dino Constructor
 
+    function Dino(species, weight, diet, where, when, fact) {
+        this.species = species,
+        this.weight = weight,
+        this.diet = diet,
+        this.where = where,
+        this.when = when,
+        this.fact = fact
+    }
 
     // Create Dino Objects
 
+    let dinos = []
+
+    dinosData.Dinos.forEach( dino => {
+        dinos.push(new Dino(...Object.values(dino)));
+    });
 
     // Create Human Object
 
+    function Human(weight, diet, where, fact) {
+        this.species = 'human',
+        this.weight = weight,
+        this.diet = diet,
+        this.where = where,
+        this.when = 'current',
+        this.fact = fact
+    }
+    
     // Use IIFE to get human data from form
 
 
