626625e2e7fda39f74a09ec9b5ffd06c43e78201
varlese <1361760+varlese@users.noreply.github.com>
2021-11-27 20:05:07 -0500

    Added final method to check each object for oldest and newest time period.

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
     const dinosJson = '{"Dinos":[{"species":"Triceratops","weight":13000,"height":114,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"First discovered in 1889 by Othniel Charles Marsh"},{"species":"Tyrannosaurus Rex","weight":11905,"height":144,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"The largest known skull measures in at 5 feet long."},{"species":"Anklyosaurus","weight":10500,"height":55,"diet":"herbavor","where":"North America","when":"Late Cretaceous","fact":"Anklyosaurus survived for approximately 135 million years."},{"species":"Brachiosaurus","weight":70000,"height":"372","diet":"herbavor","where":"North America","when":"Late Jurasic","fact":"An asteroid was named 9954 Brachiosaurus in 1991."},{"species":"Stegosaurus","weight":11600,"height":79,"diet":"herbavor","where":"North America, Europe, Asia","when":"Late Jurasic to Early Cretaceous","fact":"The Stegosaurus had between 17 and 22 seperate places and flat spines."},{"species":"Elasmosaurus","weight":16000,"height":59,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Elasmosaurus was a marine reptile first discovered in Kansas."},{"species":"Pteranodon","weight":44,"height":20,"diet":"carnivor","where":"North America","when":"Late Cretaceous","fact":"Actually a flying reptile, the Pteranodon is not a dinosaur."},{"species":"Pigeon","weight":0.5,"height":9,"diet":"herbavor","where":"World Wide","when":"Holocene","fact":"All birds are living dinosaurs."}]}';
 
-    const dinosData = JSON.parse(dinosJson);
+    const dinosData = JSON.parse( dinosJson );
 
     // Create Dino Constructor
 
-    function Dino(species, weight, height, diet, where, when, fact) {
+    function Dino( species, weight, height, diet, where, when, fact ) {
         this.species = species;
         this.weight = weight;
         this.height = height;
@@ -69,7 +69,7 @@
 
     // Create Human Object
 
-    function Human(name, height, weight, diet) {
+    function Human( name, height, weight, diet ) {
         this.species = 'human';
         this.name = name;
         this.height = parseInt( height, 10 );
@@ -82,17 +82,17 @@
     
     // Use IIFE to get human data from form
 
-    const form = document.getElementById('dino-compare');
+    const form = document.getElementById( 'dino-compare' );
 
     let humans = []
 
-    const onFormSubmit = function (event){
+    const onFormSubmit = function ( event ){
         event.preventDefault();
 
         const humanForm = new FormData(event.target);
 
         let humanEntry = {};
-        humanForm.forEach(function(value, key){
+        humanForm.forEach(function( value, key ){
             humanEntry[key] = value;
         });
 
@@ -156,18 +156,13 @@
     };
 
     // Create Dino Compare Method - Time Period
-    function compareTimePeriods( dino ) {
-        const timePeriods = [
-            "Holocene",
-            "Late Cretaceous",
-            "Late Jurasic to Early Cretaceous",
-            "Late Jurasic"
-        ];
-
-
-
+    function isOldestPeriod( item ) {
+        return item.when == "Late Jurasic";
     }
 
+    function isNewestPeriod( item ) {
+        return item.when == "Holocene";
+    }
 
     // Generate Tiles for each Dino in Array
   
